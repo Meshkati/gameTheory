2e8b3477ac02af7e65b45375fb257dd926997379
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-02-20 00:58:53 +0330

    Preparing auth backend and views

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,5 +1,30 @@
-@extends('app')
+@extends('layouts.app')
+
+@section('title')
+    بازی
+@endsection
 
 @section('content')
-    Games List!
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10 col-sm-10 col-md-6 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-0">
+                <div class="panel panel-default">
+                    <div class="panel-heading">بازی زندانی</div>
+
+                    <div class="panel-body">
+                        امتیازی کسب نکرده اید
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-10 col-sm-10 col-md-6 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-0">
+                <div class="panel panel-default">
+                    <div class="panel-heading">بازی زندانی</div>
+
+                    <div class="panel-body">
+                        امتیازی کسب نکرده اید
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
